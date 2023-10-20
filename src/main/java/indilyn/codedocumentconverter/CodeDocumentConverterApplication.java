54e21ac30ad4abbfca5ea98a078a2fbeefa1b6d1
lynsys <499804172@qq.com>
2023-10-20 15:34:02 +0800

    init project

---
+++ src/main/java/indilyn/codedocumentconverter/CodeDocumentConverterApplication.java
@@ -0,0 +1,13 @@
+package indilyn.codedocumentconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDocumentConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeDocumentConverterApplication.class, args);
+    }
+
+}
